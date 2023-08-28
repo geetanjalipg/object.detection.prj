b306b9a908d903721df92f8ec9023f19e67c5c8e
geetanjalipg <81006006+geetanjalipg@users.noreply.github.com>
2023-08-28 19:59:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function gotobedroompage(){
+    window.location="bedroom.html"
+}
+
+function gotolivingroompage(){
+    window.location="livingroom.html"
+}
+
+function gotostudyroompage(){
+    window.location="studyroom.html"
+}
+
+function gotokitchenpage(){
+    window.location="kitchen.html"
+}
